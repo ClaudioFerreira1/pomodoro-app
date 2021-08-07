0977ef2c807dd00e8d74cb2e29f0675d0c221291
Claudio Ferreira <claudioferreira.dev@gmail.com>
2021-08-07 17:32:30 -0300

    finished modal controllers

--- src/Modal.js
+++ src/Modal.js
@@ -1,8 +1,13 @@
 import { useGlobalContext } from "./context";
 import iconClose from './assets/icon-close.svg';
+import arrowUp from './assets/icon-arrow-up.svg';
+import arrowDown from './assets/icon-arrow-down.svg';
+
 
 const Modal = () => {
-  const { isModalOpen, setIsModalOpen, colorChoosed, selectedFont, pomodoroTime, shortBreakTime, longBreakTime } = useGlobalContext();
+  const { isModalOpen, setIsModalOpen, colorChoosed, selectedFont, pomodoroTime, shortBreakTime, longBreakTime, setPomodoroTime, setShortBreakTime, setLongBreakTime } = useGlobalContext();
+
+
 
   return (
     <div className={`${isModalOpen ? 'modal-overlay show-modal' : 'modal-overlay'}`}>
@@ -14,19 +19,31 @@ const Modal = () => {
 
         <main>
           <div className="main-div-modal">
-            <h4 style={{ fontFamily: selectedFont }} className="main-title-modal">TIME(MINUTES)</h4>
+            <h4 style={{ fontFamily: selectedFont }} className="main-title-modal">TIME (MINUTES)</h4>
             <div className="div-controllers-modal" style={{ fontFamily: selectedFont }}>
               <div>
-                pomodoro
-                <input type="number" min="1" max="99" step="1" value={pomodoroTime} style={{ fontFamily: selectedFont }} />
+                <h5>pomodoro</h5>
+                <input id="input-number-modal" type="number" min="1" max="60" value={pomodoroTime} style={{ fontFamily: selectedFont }} readOnly />
+                <div className="arrows-input-div">
+                  <img className={pomodoroTime === 60 ? "dissapear" : ""} src={arrowUp} alt="arrow-up" onClick={() => setPomodoroTime(pomodoroTime + 1)} />
+                  <img className={pomodoroTime === 1 ? "dissapear" : ""} src={arrowDown} alt="arrow-down" onClick={() => setPomodoroTime(pomodoroTime - 1)} />
+                </div>
               </div>
               <div>
-                short break
-                <input type="number" min="1" max="99" step="1" value={shortBreakTime} style={{ fontFamily: selectedFont }} />
+                <h5>short break</h5>
+                <input id="input-number-modal" type="number" min="1" max="99" step="1" value={shortBreakTime} style={{ fontFamily: selectedFont }} readOnly />
+                <div className="arrows-input-div">
+                  <img className={shortBreakTime === 60 ? "dissapear" : ""} src={arrowUp} alt="arrow-up" onClick={() => setShortBreakTime(shortBreakTime + 1)} />
+                  <img className={shortBreakTime === 1 ? "dissapear" : ""} src={arrowDown} alt="arrow-down" onClick={() => setShortBreakTime(shortBreakTime - 1)} />
+                </div>
               </div>
               <div>
-                long break
-                <input type="number" min="1" max="99" step="1" value={longBreakTime} style={{ fontFamily: selectedFont }} />
+                <h5>long break</h5>
+                <input id="input-number-modal" type="number" min="1" max="99" step="1" value={longBreakTime} style={{ fontFamily: selectedFont }} readOnly />
+                <div className="arrows-input-div">
+                  <img className={longBreakTime === 60 ? "dissapear" : ""} src={arrowUp} alt="arrow-up" onClick={() => setLongBreakTime(longBreakTime + 1)} />
+                  <img className={longBreakTime === 1 ? "dissapear" : ""} src={arrowDown} alt="arrow-down" onClick={() => setLongBreakTime(longBreakTime - 1)} />
+                </div>
               </div>
             </div>
             <div>FONT</div>
--- src/context.js
+++ src/context.js
@@ -4,15 +4,15 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [selectedController, setSelectedController] = useState('pomodoro')
-  const [pomodoroTime, setPomodoroTime] = useState('25')
-  const [shortBreakTime, setShortBreakTime] = useState('5')
-  const [longBreakTime, setLongBreakTime] = useState('15')
+  const [pomodoroTime, setPomodoroTime] = useState(25)
+  const [shortBreakTime, setShortBreakTime] = useState(5)
+  const [longBreakTime, setLongBreakTime] = useState(15)
   const [selectedFont, setSelectedFont] = useState('Kumbh Sans');
   const [actionText, setActionText] = useState('PAUSE');
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [colorChoosed, setColorChoosed] = useState("#F87070")
 
-  return <AppContext.Provider value={{ selectedController, setSelectedController, selectedFont, actionText, isModalOpen, setIsModalOpen, colorChoosed, pomodoroTime, shortBreakTime, longBreakTime }}>{children}</AppContext.Provider>
+  return <AppContext.Provider value={{ selectedController, setSelectedController, selectedFont, actionText, isModalOpen, setIsModalOpen, colorChoosed, pomodoroTime, shortBreakTime, longBreakTime, setPomodoroTime, setShortBreakTime, setLongBreakTime }}>{children}</AppContext.Provider>
 }
 
 const useGlobalContext = () => {
